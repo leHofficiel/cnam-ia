2480c8e7de3576e9aeeb86334872216536910956
Florian SPINDLER <83946516+Mr-Fogo@users.noreply.github.com>
2025-02-09 12:26:43 +0100

    Delete TP directory

--- TP/main.py
+++
@@ -1,7 +0,0 @@
-from src.image_processor import ImageProcessor
-
-if __name__ == "__main__":
-    chemin_images = "input_images"
-    taille_cible = 640
-    processeur = ImageProcessor(chemin_images)
-    processeur.process_folder(taille_cible)
